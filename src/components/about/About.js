b90997f58f6ebd95bf071672d2c7e4edc6bee2d2
jspieler <75164246+jspieler@users.noreply.github.com>
2023-01-06 16:48:36 +0100

    Fixed warnings due to className

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -19,7 +19,7 @@ const About = () => {
           I develop a plattform for the validation of algorithms.</p>
           <a download="" href={CV} className="button button--flex">Download CV
           <svg
-            class="button__icon"
+            className="button__icon"
             xmlns="http://www.w3.org/2000/svg"
             width="24"
             height="24"
