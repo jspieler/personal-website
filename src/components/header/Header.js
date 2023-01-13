abff5be4e20a35aea41f1fdd64c0a1ab9d8c7dad
jspieler <75164246+jspieler@users.noreply.github.com>
2023-01-13 20:05:22 +0100

    Changed Navbar logo link

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -20,7 +20,7 @@ const Header = () => {
   return (
     <header className="header">
       <nav className="nav container">
-        <a href="index.html" className="nav__logo">JS</a>
+        <a href="/" className="nav__logo">JS</a>
         <div className={toggle ? "nav__menu show-menu" : "nav__menu"}>
           <ul className="nav__list grid">
             <li className="nav__item">
