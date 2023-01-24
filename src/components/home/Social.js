a847c5ab66582f13c22994e9d3738303cb4f188f
jspieler <75164246+jspieler@users.noreply.github.com>
2023-01-24 19:09:46 +0100

    Changed Social icons in Home section

--- src/components/home/Social.js
+++ src/components/home/Social.js
@@ -3,12 +3,12 @@ import React from 'react'
 const Social = () => {
   return (
     <div className="home__social">
-      <a href="https://instagram.com/" className="home__social-icon" target="_blank" rel="noopener noreferrer">
-        <i className="uil uil-instagram"></i>
+      <a href="https://linkedin.com/" className="home__social-icon" target="_blank" rel="noopener noreferrer">
+        <i className="uil uil-linkedin"></i>
       </a>
 
-      <a href="https://dribbble.com/" className="home__social-icon" target="_blank" rel="noopener noreferrer">
-        <i className="uil uil-dribbble"></i>
+      <a href="https://xing.com/" className="home__social-icon" target="_blank" rel="noopener noreferrer">
+        <i className="bx bxl-xing"></i>
       </a>
 
       <a href="https://github.com/" className="home__social-icon" target="_blank" rel="noopener noreferrer">
