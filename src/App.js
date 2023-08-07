0e3567d219421131017516519b02592d04bb3ba4
jspieler <75164246+jspieler@users.noreply.github.com>
2023-08-07 19:10:47 +0200

    Added teaching component to page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Services from './components/services/Services';
 import Qualification from './components/qualification/Qualification';
 import Work from './components/work/Work';
 import Publications from './components/publications/Publications';
+import Teaching from './components/teaching/Teaching';
 import Testimonials from './components/testimonials/Testimonials';
 import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
@@ -28,6 +29,7 @@ const App = () => {
         <Services />
         <Qualification />
         <Work />
+        <Teaching />
         <Publications />
         <Testimonials />
         <Contact />
