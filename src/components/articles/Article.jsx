3130cff229ebfa364437be97a76bba00ae20bcc1
jspieler <75164246+jspieler@users.noreply.github.com>
2023-08-11 20:39:04 +0200

    Extended Article component

--- src/components/articles/Article.jsx
+++ src/components/articles/Article.jsx
@@ -1,15 +1,55 @@
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
 
+import Giscus from "@giscus/react";
+
+import { ThemeContext } from "../../theme/Theme";
 import "./article.css";
 
 const Article = ({ title, subtitle, children }) => {
+  const [sectionLinks, setSectionLinks] = useState([]);
+  const { theme, _ } = useContext(ThemeContext);
+
+  useEffect(() => {
+    // Create section links for all h3 headers within the article content
+    const headers = document.querySelectorAll(".article__content h3");
+    const links = Array.from(headers).map((header, index) => ({
+      title: header.textContent,
+      id: `section-${index}`,
+    }));
+    setSectionLinks(links);
+  }, []);
+
   return (
     <section className="article section" id="article">
       <h2 className="section__title">{title}</h2>
       <span className="section__subtitle">{subtitle}</span>
 
       <div className="article__container container">
-        <div className="article__data">{children}</div>
+        <div className="article__content">{children}</div>
+        <div className="article__outline">
+          <ul>
+            {sectionLinks.map((section) => (
+              <li key={section.id}>
+                <a href={`#${section.id}`}>{section.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        
+        <Giscus
+          id="comments"
+          repo="[ENTER REPO HERE]"
+          repoId="[ENTER REPO ID HERE]"
+          category="[ENTER CATEGORY NAME HERE]"
+          categoryId="[ENTER CATEGORY ID HERE]"
+          mapping="pathname"
+          reactionsEnabled="1"
+          emitMetadata="0"
+          inputPosition="top"
+          theme={theme === "dark-theme" ? "dark" : "light"}
+          lang="en"
+          loading="lazy"
+        />
       </div>
     </section>
   );
--- src/pages/IntroductionToPython.jsx
+++ src/pages/IntroductionToPython.jsx
@@ -8,8 +8,59 @@ const IntroductionToPython = () => {
       <Article
         title={"Introduction to Python"}
         subtitle={"Yet another python guide"}
-        children={"Lorem ipsum dolor sit ..."}
-      />
+      >
+        <h3 id="section-0">What is Python and why you should learn it</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+          ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+          et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+          takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
+          amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+          invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+          At vero eos et accusam et justo duo dolores et ea rebum. Stet clita
+          kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+          amet. Duis autem vel eum iriure dolor in hendrerit in vulputate velit
+          esse molestie consequat, vel illum dolore eu feugiat nulla facilisis
+          at vero eros et accumsan et iusto odio dignissim qui blandit praesent
+          luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+          nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+          volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+          ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+          Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse
+        </p>
+
+        <h3 id="section-1">Setting up a Python environment</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+          ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+          et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+          takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
+          amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+          invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+          At vero eos et accusam et justo duo dolores et ea rebum. Stet clita
+          kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+          amet. Duis autem vel eum iriure dolor in hendrerit in vulputate velit
+          esse molestie consequat, vel illum dolore eu feugiat nulla facilisis
+          at vero eros et accumsan et iusto odio dignissim qui blandit praesent
+          luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+          nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+          volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+          ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+          Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse
+        </p>
+      </Article>
     </main>
   );
 };
