c3cf6e06866bab1f59902b12d1808c53fd34ac81
jspieler <75164246+jspieler@users.noreply.github.com>
2023-09-19 11:20:15 +0200

    Fixed teaching overview routing

--- src/components/teaching/TeachingItem.jsx
+++ src/components/teaching/TeachingItem.jsx
@@ -1,16 +1,17 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const TeachingItem = ({ item }) => {
   return (
-    <a
-      href={`/teaching/${item.title.replace(/\s+/g, "-").toLowerCase()}`}
+    <Link
+      to={`${item.title.replace(/\s+/g, '-').toLowerCase()}`}
       className="teaching__button"
     >
       <div className="teaching__card" key={item.id}>
         <img src={item.image} alt="" className="teaching__img" />
         <h3 className="teaching__title">{item.title}</h3>
       </div>
-    </a>
+    </Link>
   );
 };
 
